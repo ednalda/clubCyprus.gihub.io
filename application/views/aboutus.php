cc796d7625387c2a4f4fdc07a8aceee5f120b5d2
Ednalda <ednaldafakira@gmail.com>
2015-12-06 16:29:05 -0500

    update

--- application/views/aboutus.php
+++ application/views/aboutus.php
@@ -8,8 +8,16 @@
 				      		<li>
 				      			<h2 class="about-us">About Us</h2>
 				      		    <h4>
-					      			We enable millions of commercial and social interactions among our users, between consumers and merchants, and among businesses every day.
-									We empower our customers with the fundamental infrastructure for commerce and data technology, so that they can build businesses and create value that can be shared among our ecosystem participants. We strive to expand our products and services to become central to the everyday lives of our customers. For a company that was founded in 1999, lasting at least 102 years means we will have spanned three centuries, an achievement that few companies can claim. Our culture, business models and systems are built to last, so that we can achieve sustainability in the long run.
+					      			We enable millions of commercial and social interactions among our users, 
+					      			between consumers, merchants, and among businesses every day.
+									We empower our customers with the fundamental infrastructure for 
+									commerce and data technology, so that they can build businesses 
+									and create value that can be shared among our ecosystem participants. 
+									We strive to expand our products and services to become central to the 
+									everyday lives of our customers. For a company that was founded in 1999, 
+									lasting at least 102 years means we will have spanned three centuries, 
+									an achievement that few companies can claim. Our culture, business models 
+									and systems are built to last, so that we can achieve sustainability in the long run.
 							    </h4>
 				      		</li> 
 			      	    </ul>
